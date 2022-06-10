f0d275d14d471193c06364e10fd3d125eededd3a
a-bodrova <soulwent94@gmail.com>
2022-06-11 00:51:27 +0400

    feat: add text input and checkbox components

---
+++ fe-school-vue/src/components/MyInput.vue
@@ -0,0 +1,25 @@
+<template>
+  <input class="my-input" type="text" />
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .my-input {
+    height: 29px;
+    padding: 5px 10px;
+    margin: 0;
+    @include common-text;
+    color: $color-text;
+  
+    &::placeholder {
+      @include common-text;
+      color: $color-label-text;
+    }
+  }
+</style>
--- fe-school-vue/src/components/TaskFilter.vue
+++ fe-school-vue/src/components/TaskFilter.vue
@@ -1,15 +1,18 @@
 <template>
   <section class="filter__wrapper">
     <MyCheckbox />
+    <MyInput />
   </section>
 </template>
 
 <script>
 import MyCheckbox from '@/components/MyCheckbox.vue';
+import MyInput from '@/components/MyInput.vue';
 
 export default {
   components: {
     MyCheckbox,
+    MyInput,
   }
 }
 </script>
@@ -17,6 +20,7 @@ export default {
 <style lang="scss" scoped>
   .filter__wrapper {
     display: flex;
+    gap: 10px;
   }
 
 </style>
